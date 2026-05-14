300a24a1eb66ad2d45f6649b1acfb9c984ada217
ganeshgovala <sriramganesh28@gmail.com>
2026-05-14 21:32:36 +0530

    fix: import issues

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,9 +1,11 @@
+import { test, expect } from "vitest";
 import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/vitest";
 import App from "./App";
 
 test("renders heading", () => {
   render(<App />);
-  
+
   const heading = screen.getByText(/Meet Our Team/i);
 
   expect(heading).toBeInTheDocument();
